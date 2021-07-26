4f5ee4c9a572bf0f449b05610243e14387eaac9c
zhora <zhora.mail@gmail.com>
2021-07-26 12:02:33 +0200

    Ok

---
+++ src/main/java/org/ozhorniak/devtesttask02/Main.java
@@ -0,0 +1,27 @@
+package org.ozhorniak.devtesttask02;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(">> Dev test. Task 2");
+		long startTime = System.currentTimeMillis();
+		new Main().run();
+		long allTimes = System.currentTimeMillis() - startTime;
+		System.out.println("\n>> " + allTimes + " ms.");
+	}
+
+	private void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
